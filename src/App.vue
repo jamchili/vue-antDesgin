b4fd5583caed56b10e04e92bc54d25a05777821d
jamchili <llllmc@qq.com>
2026-01-17 23:13:49 +0800

    feat: 新增代码提交规则

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+a = 10
+b = 1
+c = 22
+d = 222
+c = 333
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
